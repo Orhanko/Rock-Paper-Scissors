c48c1fbbb0f3820434741624fb5393ed87993a63
Orhan <95143599+Orhanko@users.noreply.github.com>
2022-12-07 22:40:15 +0100

    translating names of variables,functions, outputs etc. on english language

--- script.js
+++ script.js
@@ -1,140 +1,134 @@
-let mojBrojac = 0;
-let brojacKompa = 0;
-
-const divSaTekstom = document.querySelector('.text')
+let userCounter = 0;
+let computerCounter = 0;
+const divResultOutput = document.querySelector('.text')
 
 function random(){
-    let broj = Math.floor(Math.random() * 3) + 1;
-    if(broj == 1){
-        return "list";
-    }else if(broj == 2){
-        return "kamen";
+    let number = Math.floor(Math.random() * 3) + 1;
+    if(number == 1){
+        return "paper";
+    }else if(number == 2){
+        return "rock";
     }else{
-        return "makaze";
+        return "scissors";
     }
 }
 
 
 
 
-function probaDomacin(){
-    const domaci = document.querySelector('.home-container');
-    const rezultatDOmaci = document.querySelector('.homeResult')
-    rezultatDOmaci.remove();
-    //mojBrojac++;
-    let text = mojBrojac.toString();
-    const divDomaci = document.createElement('div');
-    divDomaci.classList.add('homeResult');
-    divDomaci.style.border = "1px solid black";
-    divDomaci.style.padding = "5px"
-    divDomaci.style.borderRadius = "4.5px"
-    divDomaci.textContent = text;
-    domaci.appendChild(divDomaci);
+function homeResult(){
+    const homeContainer = document.querySelector('.home-container');
+    const homeResult = document.querySelector('.homeResult')
+    homeResult.remove();
+    let userResult = userCounter.toString();
+    const divHome = document.createElement('div');
+    divHome.classList.add('homeResult');
+    divHome.style.border = "1px solid black";
+    divHome.style.padding = "5px"
+    divHome.style.borderRadius = "4.5px"
+    divHome.textContent = userResult;
+    homeContainer.appendChild(divHome);
 }
 
-    function probaGost(){
-        const gosti = document.querySelector(".guest-container");
-        const rezultatGosti = document.querySelector(".guestResult")
-        rezultatGosti.remove();
-        //brojacKompa++
-        let text = brojacKompa.toString();
-        const divGosti = document.createElement("div");
-        divGosti.classList.add('guestResult');
-        divGosti.style.border = "1px solid black";
-        divGosti.style.padding = "5px"
-        divGosti.style.borderRadius = "4.5px"
-        divGosti.textContent = text;
-        gosti.appendChild(divGosti);
+    function guestResult(){
+        const guestContainer = document.querySelector(".guest-container");
+        const guestResult = document.querySelector(".guestResult")
+        guestResult.remove();
+        let computerResult = computerCounter.toString();
+        const divGuest = document.createElement("div");
+        divGuest.classList.add('guestResult');
+        divGuest.style.border = "1px solid black";
+        divGuest.style.padding = "5px"
+        divGuest.style.borderRadius = "4.5px"
+        divGuest.textContent = computerResult;
+        guestContainer.appendChild(divGuest);
         }
 
 
 
 
-function jednaRunda() {
-    console.log("moj brojac: " + mojBrojac)
-    console.log("brojac kompa: " + brojacKompa)
-    if(mojBrojac<5 && brojacKompa<5){
-    let odabirKompa = random();
-    console.log(odabirKompa)
+function oneRound() {
     
-    let mojOdabir = ""
+    if(userCounter<5 && computerCounter<5){
+    let computerChoice = random();
+    console.log(computerChoice)
+    
+    let userChoice = ""
 
     
     
-    while(mojOdabir != "list" && mojOdabir != "kamen" && mojOdabir != "makaze"){
-        mojOdabir = prompt("Molimo unesite svoj odabir!")
+    while(userChoice != "paper" && userChoice != "rock" && userChoice != "scissors"){
+        userChoice = prompt("Please, make a choice!")
     }
-    let final = mojOdabir.toLowerCase();
-    //ovdje je bio if
-    switch(final){
-        case "makaze":
-            if (final === odabirKompa){
-                console.log("Isti odabir!");
-                textRezultat = "Nerjesena runda! Rezultat ostaje isti"
-                divSaTekstom.textContent = textRezultat
-                divSaTekstom.textContent = textRezultat
-                console.log("Rezultat je: " + mojBrojac + ":" + brojacKompa)
-            }else if(odabirKompa == "list"){
-                textRezultat = "Pobjedili ste ovu rundu, " + "Racunar je izabrao: " + odabirKompa
-                divSaTekstom.textContent = textRezultat
-                console.log("Pobjedili ste ovu rundu, " + "Racunar je izabrao: " + odabirKompa);
-                mojBrojac++;
-                probaDomacin();
-                console.log(mojBrojac);
-                console.log("Rezultat je: " + mojBrojac + ":" + brojacKompa)
-            }else if(odabirKompa == "kamen"){
-                textRezultat = "Izgubili ste rundu, " + "Racunar je izabrao: " + odabirKompa
-                divSaTekstom.textContent = textRezultat
-                console.log("Izgubili ste rundu, " + "Racunar je izabrao: " + odabirKompa);
-                brojacKompa++;
-                probaGost();
-                console.log(brojacKompa);
-                console.log("Rezultat je: " + mojBrojac + ":" + brojacKompa)
+    let userChoiceFinal = userChoice.toLowerCase();
+    
+    switch(userChoiceFinal){
+        case "scissors":
+            if (userChoiceFinal === computerChoice){
+                console.log("Same choice!");
+                resultOutput = "This round is a Tie. The result remains the same"
+                divResultOutput.textContent = resultOutput
+                
+                console.log("The Result is: " + userCounter + ":" + computerCounter)
+            }else if(computerChoice == "paper"){
+                resultOutput = "You won this round, " + "The computer chose: " + computerChoice
+                divResultOutput.textContent = resultOutput
+                console.log("You won this round, " + "The computer chose: " + computerChoice);
+                userCounter++;
+                homeResult();
+                console.log("The Result is: " + userCounter + ":" + computerCounter)
+            }else if(computerChoice == "rock"){
+                resultOutput = "You lost this round, " + "The computer chose: " + computerChoice
+                divResultOutput.textContent = resultOutput
+                console.log("You lost this round, " + "The computer chose: " + computerChoice);
+                computerCounter++;
+                guestResult(); 
+                console.log("The Result is: " + userCounter + ":" + computerCounter)
             }
             break;
-        case "list":
-            if (final === odabirKompa){
-                console.log("Isti odabir!");
-                textRezultat = "Nerjesena runda! Rezultat ostaje isti"
-                divSaTekstom.textContent = textRezultat
-                console.log("Rezultat je: " + mojBrojac + ":" + brojacKompa)
-            }else if(odabirKompa == "kamen"){
-                textRezultat = "Pobjedili ste ovu rundu, " + "Racunar je izabrao: " + odabirKompa
-                divSaTekstom.textContent = textRezultat
-                console.log("Pobjedili ste ovu rundu, " + "Racunar je izabrao: " + odabirKompa);
-                mojBrojac++;
-                probaDomacin();
-                console.log("Rezultat je: " + mojBrojac + ":" + brojacKompa)
-            }else if(odabirKompa == "makaze"){
-                console.log("Izgubili ste ovu rundu, " + "Racunar je izabrao: " + odabirKompa);
-                textRezultat = "Izgubili ste ovu rundu, " + "Racunar je izabrao: " + odabirKompa
-                divSaTekstom.textContent = textRezultat
-                brojacKompa++;
-                probaGost();
-                console.log("Rezultat je: " + mojBrojac + ":" + brojacKompa)
+        case "paper":
+            if (userChoiceFinal === computerChoice){
+                console.log("Same choice");
+                resultOutput = "This round is a Tie. The result remains the same"
+                divResultOutput.textContent = resultOutput
+                console.log("The Result is: " + userCounter + ":" + computerCounter)
+            }else if(computerChoice == "rock"){
+                resultOutput = "You won this round, " + "The computer chose: " + computerChoice
+                divResultOutput.textContent = resultOutput
+                console.log("You won this round, " + "The computer chose: " + computerChoice);
+                userCounter++;
+                homeResult();
+                console.log("The Result is: " + userCounter + ":" + computerCounter)
+            }else if(computerChoice == "scissors"){
+                console.log("You lost this round, " + "The computer chose: " + computerChoice);
+                resultOutput = "You lost this round, " + "The computer chose: " + computerChoice
+                divResultOutput.textContent = resultOutput
+                computerCounter++;
+                guestResult();
+                console.log("The Result is: " + userCounter + ":" + computerCounter)
             }
             break;
 
-        case "kamen":
-            if (final === odabirKompa){
-                console.log("Isti odabir!");
-                textRezultat = "Nerjesena runda! Rezultat ostaje isti"
-                divSaTekstom.textContent = textRezultat
-                console.log("Rezultat je: " + mojBrojac + ":" + brojacKompa)
-            }else if( odabirKompa == "makaze"){
-                textRezultat = 'Pobjedili ste ovu rundu, racunar je izabrao: ' + odabirKompa
-                divSaTekstom.textContent = textRezultat
-                console.log("Pobjedili ste ovu rundu, " + "Racunar je izabrao: " + odabirKompa);
-                mojBrojac++
-                probaDomacin();
-                console.log("Rezultat je: " + mojBrojac + ":" + brojacKompa)
-            }else if(odabirKompa == "list"){
-                textRezultat = "Izgubili ste ovu rundu, " + "Racunar je izabrao: " + odabirKompa
-                divSaTekstom.textContent = textRezultat
-                console.log("Izgubili ste ovu rundu, " + "Racunar je izabrao: " + odabirKompa);
-                brojacKompa++
-               probaGost();
-                console.log("Rezultat je: " + mojBrojac + ":" + brojacKompa)
+        case "rock":
+            if (userChoiceFinal === computerChoice){
+                console.log("Same choice!");
+                resultOutput = "This round is a Tie. The result remains the same"
+                divResultOutput.textContent = resultOutput
+                console.log("The Result is: " + userCounter + ":" + computerCounter)
+            }else if( computerChoice == "scissors"){
+                resultOutput = 'You won this round, The computer chose: ' + computerChoice
+                divResultOutput.textContent = resultOutput
+                console.log("You won this round, " + "The computer chose: " + computerChoice);
+                userCounter++
+                homeResult();
+                console.log("The Result is: " + userCounter + ":" + computerCounter)
+            }else if(computerChoice == "paper"){
+                resultOutput = "You lost this round, " + "The computer chose: " + computerChoice
+                divResultOutput.textContent = resultOutput
+                console.log("You lost this round, " + "The computer chose: " + computerChoice);
+                computerCounter++
+               guestResult();
+                console.log("The Result is: " + userCounter + ":" + computerCounter)
 
             }
             break;
@@ -143,30 +137,30 @@ function jednaRunda() {
     return;
 }
 
-if(mojBrojac == 5 && mojBrojac > brojacKompa){
-    console.log("Pobjedio je Orhan!")
-    divSaTekstom.textContent = "Vaša pobjeda"
-}else if(brojacKompa == 5 && mojBrojac < brojacKompa){
-    console.log("Pobjedio je racunar")
-    divSaTekstom.textContent = "Pobjeda kompjutera"
+if(userCounter == 5 && userCounter > computerCounter){
+    console.log("User won")
+    divResultOutput.textContent = "You won! Congratulations 🎉🎉🎉"
+}else if(computerCounter == 5 && userCounter < computerCounter){
+    console.log("Computer won")
+    divResultOutput.textContent = "The computer won 😞. Try your luck again after reseting the result"
 }
 }
     
 
 
-function ispocetka(){
-    mojBrojac = 0;
-    probaDomacin();
-    const domacin = document.querySelector(".homeResult")
-    domacin.style.backgroundColor = "#b5cdcf"
-    domacin.style.border = "none"
-    brojacKompa = 0;
-    probaGost();
-    const gost = document.querySelector(".guestResult")
-    gost.style.backgroundColor = "#b5cdcf"
-    gost.style.border = "none"
-    const divSaTekstom = document.querySelector(".text")
-    divSaTekstom.textContent = ""
+function resetResult(){
+    userCounter = 0;
+    homeResult();
+    const home = document.querySelector(".homeResult")
+    home.style.backgroundColor = "#b5cdcf"
+    home.style.border = "none"
+    computerCounter = 0;
+    guestResult();
+    const guest = document.querySelector(".guestResult")
+    guest.style.backgroundColor = "#b5cdcf"
+    guest.style.border = "none"
+    
+    divResultOutput.textContent = ""
 }
 
 
